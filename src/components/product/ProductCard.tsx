d7ff58e86192d69f69fed04448fd138128fef845
yeonilil <gwagseoyeon@gmail.com>
2025-04-10 17:46:16 +0900

    feat: 상품 공식 홈페이지 링크 연결 (예시 링크 연결)

--- src/components/product/ProductCard.tsx
+++ src/components/product/ProductCard.tsx
@@ -1,5 +1,6 @@
 'use client';
 import Image from 'next/image';
+import Link from 'next/link';
 import React, { useState } from 'react';
 
 const ProductCard = () => {
@@ -34,9 +35,12 @@ const ProductCard = () => {
       <section className="mt-1 text-sm">
         <div className="flex items-center gap-2">
           <h3 className="font-semibold"> 슈퍼포우 </h3>
-          <span className="font-bold text-gray-500 hover:text-gray-700">
+          <Link
+            href="https://m.superpaw.co.kr/"
+            className="font-bold text-gray-500 hover:text-gray-700"
+          >
             공식 {'>'}
-          </span>
+          </Link>
         </div>
         <h3 className="font-semibold">슈퍼포우 오래츄 </h3>
         <div className="flex items-center">
