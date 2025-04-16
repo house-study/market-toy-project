30512baf19fc1dd2d1e320ed8fe59852a31303ca
yeonilil <gwagseoyeon@gmail.com>
2025-04-16 22:20:20 +0900

    feat: 상품 목록 mock data 연결

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,50 +1,19 @@
 import ProductCard from '@/components/product/ProductCard';
+import { mockProducts } from '@/data/products';
 
 const Home = () => {
   return (
     <div className="mx-auto grid max-w-fit grid-cols-2 items-center justify-center gap-3 p-1 md:grid-cols-3 lg:grid-cols-5">
-      <ProductCard
-        image={'/image/product.png'}
-        name="슈퍼포우"
-        description="슈퍼포우 껌"
-        price={12800}
-        link="https://brand.naver.com/superpaw"
-      />
-      <ProductCard
-        image={'/image/product.png'}
-        name="슈퍼포우"
-        description="슈퍼포우 껌"
-        price={12800}
-        link="https://brand.naver.com/superpaw"
-      />
-      <ProductCard
-        image={'/image/product.png'}
-        name="슈퍼포우"
-        description="슈퍼포우 껌"
-        price={12800}
-        link="https://brand.naver.com/superpaw"
-      />
-      <ProductCard
-        image={'/image/product.png'}
-        name="슈퍼포우"
-        description="슈퍼포우 껌"
-        price={12800}
-        link="https://brand.naver.com/superpaw"
-      />
-      <ProductCard
-        image={'/image/product.png'}
-        name="슈퍼포우"
-        description="슈퍼포우 껌"
-        price={12800}
-        link="https://brand.naver.com/superpaw"
-      />
-      <ProductCard
-        image={'/image/product.png'}
-        name="슈퍼포우"
-        description="슈퍼포우 껌"
-        price={12800}
-        link="https://brand.naver.com/superpaw"
-      />
+      {mockProducts.map(product => (
+        <ProductCard
+          key={product.id}
+          image={product.image}
+          name={product.name}
+          description={product.description}
+          price={product.price}
+          link={product.link}
+        />
+      ))}
     </div>
   );
 };
