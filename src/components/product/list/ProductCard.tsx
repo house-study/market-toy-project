a9e6834b9e8c41996be6ad13e46ff06319f9c59b
yeonilil <gwagseoyeon@gmail.com>
2025-04-24 19:29:16 +0900

    fix: Link 태그 컴포넌트 내부로 이동

--- src/components/product/list/ProductCard.tsx
+++ src/components/product/list/ProductCard.tsx
@@ -6,6 +6,7 @@ import React, { useEffect, useState } from 'react';
 import { formatPrice } from '@/utils/format';
 
 interface ProductCardProps {
+  id: number; // 상품 ID
   image: string; // 상품 이미지 URL
   name: string; // 상품 이름
   description: string; // 상품 설명
@@ -14,6 +15,7 @@ interface ProductCardProps {
 }
 
 const ProductCard = ({
+  id,
   image,
   name,
   description,
@@ -33,50 +35,52 @@ const ProductCard = ({
     setIsLoading(false);
   };
   return (
-    <div className="flex h-[300px] flex-col">
-      <div className="group relative h-[200px] w-[200px] overflow-hidden rounded-lg shadow-md">
-        {/* 로딩 UI */}
-        {isLoading && (
-          <div
-            className="absolute inset-0 rounded-lg"
-            style={{
-              background: 'linear-gradient(90deg, #f3f4f6, #e5e7eb, #f3f4f6)',
-              backgroundSize: '200% 200%',
-              animation: 'gradient 3s ease infinite',
-            }}
+    <Link href={`/products/${id}`} key={id}>
+      <div className="flex h-[300px] flex-col">
+        <div className="group relative h-[200px] w-[200px] overflow-hidden rounded-lg shadow-md">
+          {/* 로딩 UI */}
+          {isLoading && (
+            <div
+              className="absolute inset-0 rounded-lg"
+              style={{
+                background: 'linear-gradient(90deg, #f3f4f6, #e5e7eb, #f3f4f6)',
+                backgroundSize: '200% 200%',
+                animation: 'gradient 3s ease infinite',
+              }}
+            />
+          )}
+          <Image
+            src={image}
+            alt="상품 예시 이미지"
+            width={200}
+            height={200}
+            onLoad={handleImageLoad} // 이미지가 로드되었을 때 호출
+            className="h-full w-full object-fill transition-transform duration-300 ease-in-out group-hover:scale-110"
           />
-        )}
-        <Image
-          src={image}
-          alt="상품 예시 이미지"
-          width={200}
-          height={200}
-          onLoad={handleImageLoad} // 이미지가 로드되었을 때 호출
-          className="h-full w-full object-fill transition-transform duration-300 ease-in-out group-hover:scale-110"
-        />
+        </div>
+        <section className="mt-1 w-[200px] flex-1 text-sm">
+          <div className="flex items-center gap-2">
+            <h1 className="line-clamp-1 max-w-[140px] overflow-hidden font-semibold text-ellipsis">
+              {name}
+            </h1>
+            <Link
+              href={link}
+              className="font-bold text-gray-500 hover:text-gray-700"
+            >
+              공식 {'>'}
+            </Link>
+          </div>
+          <h2 className="line-clamp-2 max-w-[180px] overflow-hidden font-semibold text-ellipsis text-gray-700">
+            {description}
+          </h2>
+          <div className="flex items-center">
+            <span className="font-semibold text-gray-700">
+              {formatPrice(price)}원
+            </span>
+          </div>
+        </section>
       </div>
-      <section className="mt-1 w-[200px] flex-1 text-sm">
-        <div className="flex items-center gap-2">
-          <h1 className="line-clamp-1 max-w-[140px] overflow-hidden font-semibold text-ellipsis">
-            {name}
-          </h1>
-          <Link
-            href={link}
-            className="font-bold text-gray-500 hover:text-gray-700"
-          >
-            공식 {'>'}
-          </Link>
-        </div>
-        <h2 className="line-clamp-2 max-w-[180px] overflow-hidden font-semibold text-ellipsis text-gray-700">
-          {description}
-        </h2>
-        <div className="flex items-center">
-          <span className="font-semibold text-gray-700">
-            {formatPrice(price)}원
-          </span>
-        </div>
-      </section>
-    </div>
+    </Link>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-import Link from 'next/link';
-
 import ProductCard from '@/components/product/list/ProductCard';
 import { mockProducts } from '@/data/products';
 
@@ -7,16 +5,15 @@ const Home = () => {
   return (
     <div className="mx-auto grid max-w-fit grid-cols-2 items-center justify-center gap-3 p-1 md:grid-cols-3 lg:grid-cols-5">
       {mockProducts.map(product => (
-        <Link href={`/products/${product.id}`} key={product.id}>
-          <ProductCard
-            key={product.id}
-            image={product.image}
-            name={product.name}
-            description={product.description}
-            price={product.price}
-            link={product.link}
-          />
-        </Link>
+        <ProductCard
+          key={product.id}
+          id={product.id}
+          image={product.image}
+          name={product.name}
+          description={product.description}
+          price={product.price}
+          link={product.link}
+        />
       ))}
     </div>
   );
