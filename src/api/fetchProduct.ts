0108232123b6395eab3e21ec13f19972e21cb8ee
yeonilil <gwagseoyeon@gmail.com>
2025-04-30 09:32:31 +0900

    fix: 환경 변수 분리

--- src/api/fetchProduct.ts
+++ src/api/fetchProduct.ts
@@ -1,5 +1,5 @@
 export const fetchProducts = async () => {
-  const res = await fetch('http://localhost:3001/products');
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/products`);
   if (!res.ok) {
     throw new Error('상품 목록 불러오기에 실패하였습니다.');
   }
