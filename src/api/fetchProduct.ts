649ca85e34d7f60c890d054cab103677c2647535
yeonilil <gwagseoyeon@gmail.com>
2025-05-01 12:21:08 +0900

    fix: 환경 변수 상수로 수정

--- src/api/fetchProduct.ts
+++ src/api/fetchProduct.ts
@@ -1,6 +1,8 @@
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
 // 상품 목록 조회
 export const fetchProducts = async (): Promise<ProductCard[]> => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/products`);
+  const res = await fetch(`${API_BASE_URL}/products`);
   if (!res.ok) {
     throw new Error('상품 목록 불러오기에 실패하였습니다.');
   }
